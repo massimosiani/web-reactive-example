1da4e809f9dc3388a0967bf3aeba4115fc5623bf
Massimo Siani <massimo.siani@openmindonline.it>
2018-09-17 10:00:51 +0200

    Init application

---
+++ src/main/java/io/github/massimosiani/webreactiveexample/WebReactiveExampleApplication.java
@@ -0,0 +1,12 @@
+package io.github.massimosiani.webreactiveexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebReactiveExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebReactiveExampleApplication.class, args);
+	}
+}
